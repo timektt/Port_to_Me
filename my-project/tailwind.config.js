cce8d8fdf2526b187126fc6047db7b137d364a01
superbear <time1710411101077@gmail.com>
2025-03-02 21:03:09 +0700

    2/3/25

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // ตรวจสอบ path ให้ตรงกับโปรเจกต์
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
