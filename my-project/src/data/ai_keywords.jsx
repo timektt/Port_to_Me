6cfb4623747bc49d3d822d72f61394c8b5f24336
superbear <time1710411101077@gmail.com>
2025-06-04 19:13:19 +0700

    04/6/25-19:13

--- my-project/src/data/ai_keywords.jsx
+++ my-project/src/data/ai_keywords.jsx
@@ -45,7 +45,8 @@ export const aiKeywords = [
     { id: 'Day44', title: 'CNN for Image Classification', path: '/courses/ai/cnn-image-classification', tags: ['Day44', 'Ai', 'beginner', 'Deep learning'] },
     { id: 'Day45', title: 'Transfer Learning with Pretrained CNNs', path: '/courses/ai/cnn-transfer-learning', tags: ['Day45', 'Ai', 'beginner', 'Deep learning'] },
      { id: 'Day46', title: 'Introduction to RNNs & Sequence Models', path: '/courses/ai/rnn-intro-sequence', tags: ['Day46', 'Ai', 'beginner', 'Deep learning'] },
-      { id: 'Day47', title: 'RNN Variants: LSTM & GRU', path: '/courses/ai/rnn-lstm-gru', tags: ['Day47', 'Ai', 'beginner', 'Deep learning'] },
+    { id: 'Day47', title: 'RNN Variants: LSTM & GRU', path: '/courses/ai/rnn-lstm-gru', tags: ['Day47', 'Ai', 'beginner', 'Deep learning'] },
+    { id: 'Day48', title: 'Applications of RNNs: Text & Time Series', path: '/courses/ai/rnn-usecases', tags: ['Day48', 'Ai', 'beginner', 'Deep learning'] },
 
 ]
 
