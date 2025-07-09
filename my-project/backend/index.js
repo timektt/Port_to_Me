0be50fff0b8bab3c03ebf161cc608da8c3c4a542
superbear <time1710411101077@gmail.com>
2025-07-09 16:06:14 +0700

    Update index.js

--- my-project/backend/index.js
+++ my-project/backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // app.listen(PORT, "0.0.0.0", () => {
 //   console.log(`🚀 Server running on http://localhost:${PORT}`);
 // });
-
+console.log("📦 DATABASE_URL:", process.env.DATABASE_URL);
 
 // ✅ PostgreSQL connection
 const pool = new Pool({
