8fc91ac97436be65029c221df542def819a35aad
superbear <time1710411101077@gmail.com>
2025-07-09 16:19:05 +0700

    fix: add verifyFirebaseToken again

--- my-project/middlewares/verifyFirebaseToken.js
+++ my-project/middlewares/verifyFirebaseToken.js
@@ -1,4 +1,4 @@
-const admin = require("../firebaseAdmin");
+const admin = require("../backend/firebaseAdmin");
 
 const verifyFirebaseToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
